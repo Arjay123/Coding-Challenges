be3aa39cc816483b51d1ae06229d9cac5cc71bec
Arjay123 <arjaynguyen123@hotmail.com>
2015-05-26 10:29:32 -0700

    Card implemented

--- DailyProgrammer/DP216E.py
+++ DailyProgrammer/DP216E.py
@@ -1,1 +1,15 @@
 #http://www.reddit.com/r/dailyprogrammer/comments/378h44/20150525_challenge_216_easy_texas_hold_em_1_of_3/
+class Card:
+	suit = ''
+	value = ''
+
+	def __init__(self, suit, value):
+		self.suit = suit
+		self.value = value
+
+	def printCard(self):
+		print(self.value + ' of ' + self.suit)
+
+
+#card = Card("Spades", "10")
+#card.printCard()
